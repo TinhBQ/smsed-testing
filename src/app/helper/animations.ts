b8ac081e84a5b5d907c785cd452771c092d367be
TinhBQ <81704759+TinhBQ@users.noreply.github.com>
2023-12-19 22:22:25 +0700

    a

--- src/app/helper/animations.ts
+++ src/app/helper/animations.ts
@@ -23,7 +23,7 @@ export const largeToSmall = trigger('toggleCollapsed', [
   transition('large => small', animate('0.35s ease-out')),
 ]);
 
-// skks
+// skksslsllsslsl
 export const smallToLarge = trigger('toggleCollapsed', [
   state(
     'large',
